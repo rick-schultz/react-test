b2cb57c362d2de5ec25b5caf655f95fab72f6693
Rick Schultz <rick-schultz@hotmail.com>
2021-08-04 09:46:25 -0300

    first commit

---
+++ react-test/server/routes/Posts.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { Posts } = require('../models');
+
+router.get('/', async (req, res) => {
+  const listOfPosts = await Posts.findAll();
+  res.json(listOfPosts);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const post = req.body;
+    await Posts.create(post);
+    res.json(post);
+  } catch (error) {
+    res.send(error);
+  }
+});
+module.exports = router;
